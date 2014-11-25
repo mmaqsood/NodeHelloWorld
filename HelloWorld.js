c932bbf3235555438f532404d3719560681a0ab8
mmaqsood <mmaqsood99@gmail.com>
2014-11-25 14:23:06 -0500

    Create HelloWorld.js

---
+++ HelloWorld.js
@@ -0,0 +1,14 @@
+module.exports = {
+	sayHello: function(cb){
+		var http = require('http');
+
+		//Create the server
+		http.createServer(function (req, res) {
+		  res.writeHead(200, {'Content-Type': 'text/plain'});
+		  res.end('Hello World\n');
+		}).listen(3000);
+
+		//Send back the server
+		cb(http);
+	}
+}
